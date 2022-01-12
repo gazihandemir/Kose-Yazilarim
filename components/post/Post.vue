cfddae780d46c03b24ee20f3d9255a877276f8f4
gazihandemir <gazihand@gmail.com>
2022-01-12 21:27:34 +0300

    post detay sayfası yapıldı

--- components/post/Post.vue
+++ components/post/Post.vue
@@ -7,7 +7,12 @@
         Some quick example text to build on the card title and make up the bulk
         of the card's content.
       </p>
-      <a href="#" class="btn btn-outline-info btn-sm">Görüntüle</a>
+      <a
+        href="#"
+        @click.prevent="$router.push(`/posts/1`)"
+        class="btn btn-outline-info btn-sm"
+        >Görüntüle</a
+      >
       <a href="#" class="btn btn-outline-warning btn-sm">Düzenle</a>
       <a href="#" class="btn btn-outline-danger btn-sm">Sil</a>
     </div>
--- pages/posts/_postId/index.vue
+++ pages/posts/_postId/index.vue
@@ -1,3 +1,12 @@
-<template></template>
-<script></script>
+<template>
+  <PostDetail />
+</template>
+<script>
+import PostDetail from "@/components/post/PostDetail";
+export default {
+  components: {
+    PostDetail,
+  },
+};
+</script>
 <style scoped></style>
