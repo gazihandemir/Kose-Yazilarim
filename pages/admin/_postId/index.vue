4e07820d74e6a9976fd968eaea781233366fed78
gazihandemir <gazihand@gmail.com>
2022-01-24 14:24:11 +0300

    post düzenle firebase ile yapıldı

--- pages/admin/_postId/index.vue
+++ pages/admin/_postId/index.vue
@@ -1,13 +1,46 @@
 <!-- pages -> admin -> _postId -> index.vue -->
 <template>
-  <PostForm :is-update="true" :post="loadedPost" />
+  <PostForm
+    @submit="updatePost($event)"
+    :is-update="true"
+    :post="fetchedPost"
+  />
 </template>
 <script>
+import axios from "axios";
 import PostForm from "@/components/admin/PostForm";
 export default {
   components: {
     PostForm,
   },
+  asyncData(context) {
+    return axios
+      .get(
+        `https://kose-yazilarim-d82f6-default-rtdb.firebaseio.com/posts/${context.params.postId}.json`
+      )
+      .then((response) => {
+        return {
+          fetchedPost: response.data,
+        };
+      });
+  },
+  methods: {
+    updatePost(editedPost) {
+      axios
+        .put(
+          `https://kose-yazilarim-d82f6-default-rtdb.firebaseio.com/posts/${this.$route.params.postId}.json`,
+          editedPost
+        )
+        .then((response) => {
+          this.$router.push("/admin");
+        })
+        .catch((e) => console.log(e));
+    },
+  },
+};
+</script>
+
+<!--
   data() {
     return {
       loadedPost: {
@@ -19,5 +52,5 @@ export default {
       },
     };
   },
-};
-</script>
+
+ -->
