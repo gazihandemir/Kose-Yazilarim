66b7502dc01b6a08b8d906436b2ebc986675a263
gazihandemir <gazihand@gmail.com>
2022-01-24 12:59:13 +0300

    firebase ilk post işlemi gerçekleştirildi.

--- pages/admin/new-post.vue
+++ pages/admin/new-post.vue
@@ -3,6 +3,7 @@
   <PostForm @submit="savePost($event)" />
 </template>
 <script>
+import axios from "axios";
 import PostForm from "@/components/admin/PostForm";
 export default {
   components: {
@@ -11,6 +12,15 @@ export default {
   methods: {
     savePost(post) {
       console.log(post);
+      axios
+        .post(
+          "https://kose-yazilarim-d82f6-default-rtdb.firebaseio.com/posts.json",
+          post
+        )
+        .then((res) => {
+          console.log(res);
+          this.$router.push("/admin");
+        });
     },
   },
 };
