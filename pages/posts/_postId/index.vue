12bd5dedd67e4de17e77ca87b025f6a5dabe18b4
gazihandemir <gazihand@gmail.com>
2022-01-24 14:15:07 +0300

    kose yazisi detail sayfası firebase aracılığı ile yapıldı.

--- pages/posts/_postId/index.vue
+++ pages/posts/_postId/index.vue
@@ -1,18 +1,35 @@
-<!-- pages -> posts ->  _postId ->index.vue  -->
+<!-- pages -> posts ->  _postId -> index.vue  -->
 
 <template>
-  <PostDetail :post="fetchPost" />
+  <PostDetail :post="fetchedPost" />
 </template>
 <script>
+import axios from "axios";
 import PostDetail from "@/components/post/PostDetail";
 export default {
   components: {
     PostDetail,
   },
+  asyncData(context) {
+    return axios
+      .get(
+        `https://kose-yazilarim-d82f6-default-rtdb.firebaseio.com/posts/${context.params.postId}.json`
+      )
+      .then((response) => {
+        return {
+          fetchedPost: response.data,
+        };
+      });
+  },
+};
+</script>
+<style scoped></style>
+
+<!-- 
   asyncData(context, callback) {
     setTimeout(() => {
       callback(null, {
-        fetchPost: {
+        fetchedPost: {
           id: 1,
           title: "title 1 id'sine budur : (" + context.params.postId + ")",
           subTitle: "sub title 1",
@@ -22,6 +39,5 @@ export default {
       });
     }, 1500);
   },
-};
-</script>
-<style scoped></style>
+
+-->
